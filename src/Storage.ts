e154f3f0be28dda38c4778e9e3fb956cdd549a0b
Javier <xgc1986@gmail.com>
2021-07-03 23:08:09 +0200

    migrate to typescript

--- src/Storage.ts
+++ src/Storage.ts
@@ -2,16 +2,36 @@ export class Storage {
 
     public static readonly PROGRESS_SUMMARY: string = 'settings-grammar';
 
+    private static data: {[key: string]: number | boolean | string} = {};
+
+    private static hasInit: boolean = false;
+
+    private static init() {
+        if (!Storage.hasInit) {
+            Storage.hasInit = true;
+            try {
+                Storage.data = JSON.parse(localStorage['bpp'] ?? {})
+            } catch (e) { 
+                Storage.data = {};
+            }
+        }
+    }
+
     public static saveBoolean(key: string, value: boolean) {
-        localStorage[key] = value;
+        try {
+            Storage.init();
+            Storage.data[key] = value;
+            localStorage['bpp'] = JSON.stringify(Storage.data);
+        } catch (e) {}
     }
 
     public static loadBoolean(key: string, def: boolean): boolean {
-        if (localStorage[key] === 'true') {
+        Storage.init();
+        if (Storage.data[key] === true) {
             return true;
         }
 
-        if (localStorage[key] === 'false') {
+        if (Storage.data[key] === false) {
             return false;
         }
 
